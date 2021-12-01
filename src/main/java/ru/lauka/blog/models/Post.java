bffb7635eaf324a42f1a758dcb827321bf5e390c
lay4onok <dvsurganov@gmail.com>
2021-12-01 15:46:27 +0300

    Add articles

--- src/main/java/ru/lauka/blog/models/Post.java
+++ src/main/java/ru/lauka/blog/models/Post.java
@@ -11,9 +11,17 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String title, anons, full_text;
     private int views;
+
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
+
+    public Post() {
+    }
 
     public Long getId() {
         return id;
