34d97037fbe6df8cf1f84a2db3f1a74a904cb6aa
lay4onok <dvsurganov@gmail.com>
2021-12-02 14:54:43 +0300

    Edit Delete Count of views

--- src/main/java/ru/lauka/blog/controllers/BlogController.java
+++ src/main/java/ru/lauka/blog/controllers/BlogController.java
@@ -44,11 +44,47 @@ public class BlogController {
             return "redirect:/blog";
         }
 
+        Post postId = postRepository.findById(id).orElseThrow();
+        postId.setViews(postId.getViews() + 1);
+        postRepository.save(postId);
+
         Optional<Post> post = postRepository.findById(id);
         ArrayList<Post> res = new ArrayList<>();
         post.ifPresent(res :: add);
         model.addAttribute("post", res);
         return "blog-details";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model) {
+        if(!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res :: add);
+        model.addAttribute("post", res);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/delete")
+    public String blogPostDelete(@PathVariable(value = "id") long id, Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+
+        return "redirect:/blog";
     }
 
 }
--- src/main/java/ru/lauka/blog/models/Post.java
+++ src/main/java/ru/lauka/blog/models/Post.java
@@ -4,9 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Optional;
 
 @Entity
-public class Post {
+public class Post{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
